cb176fbde65d9a108296f70a65289e1e03bae566
a-staab <astaab@gmail.com>
2018-10-20 16:41:05 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+import App from "./App.jsx"
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("app-root")
+	)
 // Exercise 1: Render It
 	// import React
 	// import ReactDOM
