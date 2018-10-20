5fc63bab0b4f9ce1e26fee165a691a4555a3837a
a-staab <astaab@gmail.com>
2018-10-20 16:41:38 -0700

    Wrote index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,3 @@ ReactDOM.render(
 	<App />,
 	document.getElementById("app-root")
 	)
-// Exercise 1: Render It
-	// import React
-	// import ReactDOM
-	// import App
-	// import styles
-	// render App component into app-root element
-
-
